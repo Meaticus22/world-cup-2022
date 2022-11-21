8e8d869c49754eb2c571c043ffdba3fb41432fb6
Meaticus22 <117058929+Meaticus22@users.noreply.github.com>
2022-11-21 16:29:04 -0500

    create te

---
+++ .src/app/content-containers/constants/teams.ts
@@ -0,0 +1,220 @@
+import { Group, Team } from '../content-container.store';
+
+export const GROUP_A: Team[] = [
+  {
+    id: 'qatar',
+    name: 'Qatar',
+    group: 'A',
+    emojiFlag: '🇶🇦',
+  },
+  {
+    id: 'ecuador',
+    name: 'Ecuador',
+    group: 'A',
+    emojiFlag: '🇪🇨',
+  },
+  {
+    id: 'senegal',
+    name: 'Senegal',
+    group: 'A',
+    emojiFlag: '🇸🇳',
+  },
+  {
+    id: 'netherlands',
+    name: 'Holanda',
+    group: 'A',
+    emojiFlag: '🇳🇱',
+  },
+];
+
+export const GROUP_B: Team[] = [
+  {
+    id: 'england',
+    name: 'Inglaterra',
+    group: 'B',
+    emojiFlag: '🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+  },
+  {
+    id: 'iran',
+    name: 'Iran',
+    group: 'B',
+    emojiFlag: '🇮🇷',
+  },
+  {
+    id: 'usa',
+    name: 'EEUU',
+    group: 'B',
+    emojiFlag: '🇺🇸',
+  },
+  {
+    id: 'wales',
+    name: 'Gales',
+    group: 'B',
+    emojiFlag: '🏴󠁧󠁢󠁷󠁬󠁳󠁿',
+  },
+];
+
+export const GROUP_C: Team[] = [
+  {
+    id: 'argentina',
+    name: 'Argentina',
+    group: 'C',
+    emojiFlag: '🇦🇷',
+  },
+  {
+    id: 'saudi-arabia',
+    name: 'Arabia S.',
+    group: 'C',
+    emojiFlag: '🇸🇦',
+  },
+  {
+    id: 'mexico',
+    name: 'Mexico',
+    group: 'C',
+    emojiFlag: '🇲🇽',
+  },
+  {
+    id: 'poland',
+    name: 'Polonia',
+    group: 'C',
+    emojiFlag: '🇵🇱',
+  },
+];
+
+export const GROUP_D: Team[] = [
+  // teams are france, australia, denmark,tunisia
+  {
+    id: 'france',
+    name: 'Francia',
+    group: 'D',
+    emojiFlag: '🇫🇷',
+  },
+  {
+    id: 'australia',
+    name: 'Australia',
+    group: 'D',
+    emojiFlag: '🇦🇺',
+  },
+  {
+    id: 'denmark',
+    name: 'Dinamarca',
+    group: 'D',
+    emojiFlag: '🇩🇰',
+  },
+  {
+    id: 'tunisia',
+    name: 'Tunez',
+    group: 'D',
+    emojiFlag: '🇹🇳',
+  },
+];
+
+export const GROUP_E: Team[] = [
+  {
+    id: 'spain',
+    name: 'España',
+    group: 'E',
+    emojiFlag: '🇪🇸',
+  },
+  {
+    id: 'costa-rica',
+    name: 'Costa Rica',
+    group: 'E',
+    emojiFlag: '🇨🇷',
+  },
+  {
+    id: 'germany',
+    name: 'Alemania',
+    group: 'E',
+    emojiFlag: '🇩🇪',
+  },
+  {
+    id: 'japan',
+    name: 'Japon',
+    group: 'E',
+    emojiFlag: '🇯🇵',
+  },
+];
+
+export const GROUP_F: Team[] = [
+  // teams are belgium, canada, morocco and croatia
+  {
+    id: 'belgium',
+    name: 'Belgica',
+    group: 'F',
+    emojiFlag: '🇧🇪',
+  },
+  {
+    id: 'canada',
+    name: 'Canada',
+    group: 'F',
+    emojiFlag: '🇨🇦',
+  },
+  {
+    id: 'morocco',
+    name: 'Marruecos',
+    group: 'F',
+    emojiFlag: '🇲🇦',
+  },
+  {
+    id: 'croatia',
+    name: 'Croacia',
+    group: 'F',
+    emojiFlag: '🇭🇷',
+  },
+];
+
+export const GROUP_G: Team[] = [
+  // teams are brazil, serbia, switzerland, cameroon
+  {
+    id: 'brazil',
+    name: 'Brazil',
+    group: 'G',
+    emojiFlag: '🇧🇷',
+  },
+  {
+    id: 'serbia',
+    name: 'Serbia',
+    group: 'G',
+    emojiFlag: '🇷🇸',
+  },
+  {
+    id: 'switzerland',
+    name: 'Suiza',
+    group: 'G',
+    emojiFlag: '🇨🇭',
+  },
+  {
+    id: 'cameroon',
+    name: 'Camerun',
+    group: 'G',
+    emojiFlag: '🇨🇲',
+  },
+];
+
+export const GROUP_H: Team[] = [
+  {
+    id: 'portugal',
+    name: 'Portugal',
+    group: 'H',
+    emojiFlag: '🇵🇹',
+  },
+  {
+    id: 'ghana',
+    name: 'Ghana',
+    group: 'H',
+    emojiFlag: '🇬🇭',
+  },
+  {
+    id: 'uruguay',
+    name: 'Uruguay',
+    group: 'H',
+    emojiFlag: '🇺🇾',
+  },
+  {
+    id: 'south-korea',
+    name: 'Corea del Sur',
+    group: 'H',
+    emojiFlag: '🇰🇷',
+  },
+];
