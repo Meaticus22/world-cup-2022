b6a28decc454c38a018369deacb91a087a4e8782
Meaticus22 <117058929+Meaticus22@users.noreply.github.com>
2022-11-21 10:34:58 -0500

    create link.ts
    UPDATED LINK.TS

---
+++ .src/app/content-containers/constants/link.ts
@@ -0,0 +1,116 @@
+import { Edge } from '@swimlane/ngx-graph';
+
+export const LINKS: Edge[] = [
+  {
+    id: 'aa12',
+    source: 'A1vsB2',
+    target: 'Cuartos_1',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'bb13',
+    source: 'C1vsD2',
+    target: 'Cuartos_1',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'kg14',
+    source: 'E1vsF2',
+    target: 'Cuartos_2',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'et15',
+    source: 'G1vsH2',
+    target: 'Cuartos_2',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'jk16',
+    source: 'Cuartos_1',
+    target: 'Semi_1',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'aq17',
+    source: 'Cuartos_2',
+    target: 'Semi_1',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'ae19',
+    source: 'Semi_1',
+    target: 'Final',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'ml18',
+    source: 'Final',
+    target: 'Semi_2',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'nj12',
+    source: 'Cuartos_3',
+    target: 'A2vsB1',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'rf13',
+    source: 'Cuartos_3',
+    target: 'C2vsD1',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'yhe5',
+    source: 'Cuartos_4',
+    target: 'E2vsF1',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'awds',
+    source: 'Cuartos_4',
+    target: 'G2vsH1',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'cgt7',
+    source: 'Semi_2',
+    target: 'Cuartos_3',
+    data: {
+      color: '',
+    },
+  },
+  {
+    id: 'cf57',
+    source: 'Semi_2',
+    target: 'Cuartos_4',
+    data: {
+      color: '',
+    },
+  },
+];
